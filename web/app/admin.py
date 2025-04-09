fdbabe21d997a2bd2f2e392951184c2bc9fef424
Ruslan Zabarov <ruszabarov@gmail.com>
2025-04-08 21:53:31 -0400

    add lampi to admin

--- web/app/admin.py
+++ web/app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from app.models import User, SensorReading
+from app.models import SensorReading, Lampi
 
 admin.site.register(SensorReading)
+admin.site.register(Lampi)
